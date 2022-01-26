afc29e0d6632f744ebee2489b38b03249b842091
Vafflee <70109947+Vafflee@users.noreply.github.com>
2022-01-26 20:46:42 +0300

    Add files via upload

---
+++ src/js/01_main.js
@@ -0,0 +1,8 @@
+// const videoFrame = document.getElementById("main-video");
+// console.log(videoFrame);
+// console.log(videoFrame.width, videoFrame.height);
+// const content = videoFrame.closest(".content");
+// console.log(content);
+// contentStyle = getComputedStyle(content);
+// console.log(parseInt(contentStyle.width) / 2);
+// videoFrame.width = parseInt(contentStyle.width) / 2;
